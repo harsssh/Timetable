5d16a65d2868fa8aa4fbbe590b6ea19e39f2ed4e
harsssh <mizuki.k.aa@m.titech.ac.jp>
2021-10-01 09:12:41 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'space-before-function-paren': [2, { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'none',
+        varsIgnorePattern: '^_' // 変数名が '_' で始まる場合は許可
+      }
+    ],
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error'
+  }
+}
