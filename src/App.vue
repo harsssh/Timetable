bc6819cd4ae6870d6e13a753c83d7ce0add02066
harsssh <mizuki.k.aa@m.titech.ac.jp>
2021-10-01 22:13:01 +0900

    component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
-  <div id='app'>
-    <img alt='Vue logo' src='./assets/logo.png'>
-    <HelloWorld msg='Welcome to Your Vue.js + TypeScript App' />
+  <div id="app">
+    <TimeTable />
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import TimeTable from './components/TimeTable'
 
 @Component({
   components: {
-    HelloWorld
-  }
+    TimeTable,
+  },
 })
-export default class App extends Vue {
-}
+export default class App extends Vue {}
 </script>
 
 <style>
@@ -23,8 +21,6 @@ export default class App extends Vue {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TimeTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="time-table-conteiner">
+    <h1>2021 3Q</h1>
+    <Week :week="weekDays[0]"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Week from './Week'
+
+@Component({
+  components: {
+    Week,
+  },
+})
+export default class TimeTable extends Vue {
+  weekDays = ['月', '火', '水', '木', '金']
+}
+</script>
+
+<style scoped></style>
