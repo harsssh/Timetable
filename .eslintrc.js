e82c21e32514a47349fe9a59c29e50ca642ea229
harsssh <mizuki.k.aa@m.titech.ac.jp>
2021-10-01 22:11:28 +0900

    Revert "Revert "update""
    This reverts commit f7f7e57c3f414bf2455a63567aeb98a2ada30fb8.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true,
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    parser: '@typescript-eslint/parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': 'off',
-    'space-before-function-paren': [2, { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
+    'space-before-function-paren': [
+      2,
+      { anonymous: 'never', named: 'never', asyncArrow: 'always' },
+    ],
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
         args: 'none',
-        varsIgnorePattern: '^_' // 変数名が '_' で始まる場合は許可
-      }
+        varsIgnorePattern: '^_', // 変数名が '_' で始まる場合は許可
+      },
     ],
     'vue/array-bracket-spacing': 'error',
     'vue/arrow-spacing': 'error',
@@ -32,6 +38,6 @@ module.exports = {
     'vue/component-name-in-template-casing': 'error',
     'vue/eqeqeq': 'error',
     'vue/key-spacing': 'error',
-    'vue/match-component-file-name': 'error'
-  }
+    'vue/match-component-file-name': 'error',
+  },
 }
